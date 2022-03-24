0f59f1832c3cef419313fabed5d43c6fe4538c6d
michele camera <michy.cam95@gmail.com>
2022-03-24 15:04:41 +0100

    creation exercise

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,13 @@
 console.log("js-ok")
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            testo: 'Hello World',
+            image: 'gatto.jpg',
+        },
+    }
+);
+
 
 
